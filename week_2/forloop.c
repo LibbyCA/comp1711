5206c3686ff26c3d64e655e823fa6d5ab2c91bb6
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-13 09:46:06 +0000

    save

---
+++ week_2/forloop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a;
+
+    for (a=0; a < 5; a++)
+    {
+        printf("a is equal to %d\n",a);
+    }
+    printf("I've finished the loop and a is equal to %d\n",a);
+
+    return 0;
+
+
+}
---
+++ week_2/oddloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int a;
+
+    for (a=1; a < 10; a+=2)
+    {
+        printf ("value is %d\n",a);
+    }
+    return 0;
+}
