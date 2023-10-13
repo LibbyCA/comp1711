8f1328b47ed18f6e03022db5501c2eeaef52faf1
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-13 09:24:25 +0000

    moving files

---
+++ week_2/factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, factorial=1;
+    
+    
+    scanf("%d",&n);
+
+    for(int i=1; i<=n;i++){
+        factorial*=i;
+    }
+
+    printf("Factorial is %d",factorial);
+
+    return 0;
+    
+}
