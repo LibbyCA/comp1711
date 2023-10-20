6eb7246776293a110f0ed335dbf13dd7d00cfa20
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-20 09:38:37 +0000

    save

--- week_3/functions2.c
+++ week_3/functions2.c
@@ -12,5 +12,5 @@ int main ()
     int y = 2;
     float z = sum(5, y); // calling our new function
 
-    printf ("The sum of 5 and %d is %d\n", y, z);
+    printf ("The sum of 5 and %d is %f\n", y, z);
 }
