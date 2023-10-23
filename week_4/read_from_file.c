eace11d360cf9754f38212d0e74050d448535c4a
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-23 11:26:36 +0000

    header

--- week_4/read_from_file.c
+++ week_4/read_from_file.c
@@ -14,11 +14,21 @@ int main() {
     // a string which will hold each line as it's read in
     char line_buffer[buffer_size];
 
+    // set up arbitrary number of 1000
+    int numbers[1000];
+
+    // count number of lines
+    int line_number = 0;
+
     // while there is still stuff in the file
     while (fgets(line_buffer, buffer_size, file) != NULL) {
         printf("%d\n", atoi(line_buffer));
+        numbers[line_number] = atoi(line_buffer);
+        line_number++;
     }
 
+    printf("There were %d lines\n",line_number);
+    
     fclose(file);
     return 0;
 }
