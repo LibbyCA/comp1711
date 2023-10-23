d17eb69cd4f645ccf98c732b79396c91931665a2
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-23 10:39:32 +0000

    save2

---
+++ week_4/challenge1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char *filename = "data.txt";
+
+    FILE *file = fopen(filename, "w"); // or "a", "w+", "a+"
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int number;
+    int num_lines;
+
+    printf("How many numbers would you like to enter");
+    scanf ("%d", &num_lines);
+    
+    printf("Type %d numbers: ", num_lines);
+    
+    for (int i = 0; i < num_lines; i++)
+    {
+        scanf("%d", &number);
+        fprintf(file, "%d\n", number);
+    }
+
+    fclose(file);
+    return 0;
+}
---
+++ week_4/challenge2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char *filename = "data.txt";
+
+    FILE *file = fopen(filename, "w"); // or "a", "w+", "a+"
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int number;
+    int num_lines = 10;
+    int total = 0;
+    float mean;
+    
+    printf("Type %d numbers: ", num_lines);
+    
+    for (int i = 0; i < num_lines; i++)
+    {
+        scanf("%d", &number);
+        total = total + number;
+        fprintf(file, "%d\n", number);
+    }
+
+    mean = number / 10;
+    printf("The mean is %f",mean);
+
+    fclose(file);
+    return 0;
+}
